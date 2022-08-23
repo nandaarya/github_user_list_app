ef37a9739bd6f9d895bf7c13b496b41f6c8576c3
Nanda Arya Putra <nandaarya404@gmail.com>
2022-08-23 20:48:32 +0700

    initial commit

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:github_user_list_app/screen/main_screen.dart';
 import 'package:splashscreen/splashscreen.dart';
 
 class SplashScreenPage extends StatelessWidget {
