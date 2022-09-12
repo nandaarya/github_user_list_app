d953780d1477ad5f97a51cd40df081398d4a7b7a
Nanda Arya Putra <nandaarya404@gmail.com>
2022-09-12 20:44:54 +0700

    (+) add const

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Github User List',
       debugShowCheckedModeBanner: false,
     );
