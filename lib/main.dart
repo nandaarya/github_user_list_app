d23cba6468d62d286bc0a8b36c4a04153fea239c
Nanda Arya Putra <nandaarya404@gmail.com>
2023-01-21 23:02:08 +0700

    add AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
 
-void main () => runApp(const MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Github User List',
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Github User List App",
+            style: TextStyle(color: Colors.black),
+          ),
+        ),
+        body: Center(
+          child: Column(
+
+          ),
+        ),
+      ),
     );
   }
 }
-
